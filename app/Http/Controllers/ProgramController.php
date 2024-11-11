210c65cc0e832fade458189e7e639d03a99c09c3
Lerian Febriana <kanglerian@gmail.com>
2024-11-11 15:58:57 +0700

    update

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -125,18 +125,95 @@ class ProgramController extends Controller
      * Show the form for editing the specified resource.
      */
 
-    public function edit(Program $program)
+    public function edit($id)
     {
-        //
+        $program = Program::findOrFail($id);
+        return view('programs.edit', compact('program'));
     }
 
     /**
      * Update the specified resource in storage.
      */
 
-    public function update(Request $request, Program $program)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate(
+            [
+                'code' => 'required|max:10|min:4|unique:programs,code,' . $id,
+                'title' => 'required|max:100|min:2',
+                'campus' => 'required|max:100|min:2',
+                'level' => 'required|max:100|min:2',
+                'type' => 'required|max:10|min:1',
+                'status' => 'boolean',
+                'image' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+            ],
+            [
+                'code.required' => 'Code is required',
+                'code.max' => 'Code should not be more than 10 characters',
+                'code.min' => 'Code should not be less than 4 characters',
+                'code.unique' => 'Code already exists',
+                'title.required' => 'Title is required',
+                'title.max' => 'Title should not be more than 100 characters',
+                'title.min' => 'Title should not be less than 10 characters',
+                'campus.required' => 'Campus is required',
+                'campus.max' => 'Campus should not be more than 100 characters',
+                'campus.min' => 'Campus should not be less than 2 characters',
+                'level.required' => 'Level is required',
+                'level.max' => 'Level should not be more than 100 characters',
+                'level.min' => 'Level should not be less than 2 characters',
+                'type.required' => 'Type is required',
+                'type.max' => 'Type should not be more than 10 characters',
+                'type.min' => 'Type should not be less than 1 characters',
+                'status.boolean' => 'Status should be boolean',
+                'image.image' => 'Image should be an image',
+            ],
+        );
+
+        try {
+            $data = [
+                'code' => $request->code,
+                'title' => $request->title,
+                'campus' => $request->campus,
+                'level' => $request->level,
+                'type' => $request->type,
+                'status' => 1,
+            ];
+
+            $program = Program::findOrFail($id);
+
+            if($request->hasFile('image')){
+                if ($program->image && Storage::disk('public')->exists($program->image)) {
+                    Storage::disk('public')->delete($program->image);
+                }
+
+                $path = $request->file('image')->store('images/programs', 'public');
+                $data['image'] = $path;
+            }
+            $program->update($data);
+
+            return redirect()
+                ->route('programs.index')
+                ->with([
+                    'message' => 'Program updated successfully',
+                    'alert-type' => 'success',
+                ]);
+        } catch (\Illuminate\Database\QueryException $e) {
+            if ($e->errorInfo[1] == 1062) {
+                return redirect()
+                    ->route('programs.index')
+                    ->with([
+                        'message' => 'Code already exists. Please use a different code.',
+                        'alert-type' => 'failed',
+                    ]);
+            }
+
+            return redirect()
+                ->route('programs.index')
+                ->with([
+                    'message' => 'An unexpected error occurred. Please try again later.',
+                    'alert-type' => 'failed',
+                ]);
+        }
     }
 
     /**
---
+++ resources/views/programs/edit.blade.php
@@ -0,0 +1,147 @@
+<x-app-layout>
+    <x-slot name="header">
+
+
+        <nav class="flex" aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-3">
+                <li class="inline-flex items-center">
+                    <a href="{{ route('programs.index') }}"
+                        class="inline-flex items-center gap-2 text-sm font-medium text-gray-700 hover:text-gray-800 transition-all ease-in-out">
+                        <i class="fa-solid fa-book"></i>
+                        <span>Programs</span>
+                    </a>
+                </li>
+                <li aria-current="page">
+                    <div class="flex items-center gap-1">
+                        <i class="fa-solid fa-angle-right text-gray-300"></i>
+                        <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2">Edit</span>
+                    </div>
+                </li>
+            </ol>
+        </nav>
+
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-4">
+            @if (session('alert-type') == 'success')
+                <div id="alert" class="flex items-center p-4 text-emerald-50 rounded-xl bg-emerald-500"
+                    role="alert">
+                    <i class="fa-solid fa-circle-info"></i>
+                    <span class="sr-only">Info</span>
+                    <div class="ms-3 text-sm font-medium">
+                        {{ session('message') }}
+                    </div>
+                    <button type="button" onclick="document.getElementById('alert').style.display = 'none';"
+                        class="text-emerald-50 hover:text-emerald-100 transition-all ease-in-out ms-auto">
+                        <i class="fa-solid fa-circle-xmark"></i>
+                    </button>
+                </div>
+            @endif
+
+            @if (session('alert-type') == 'failed')
+                <div id="alert" class="flex items-center p-4 text-emerald-50 rounded-xl bg-red-500" role="alert">
+                    <i class="fa-solid fa-circle-info"></i>
+                    <span class="sr-only">Info</span>
+                    <div class="ms-3 text-sm font-medium">
+                        {{ session('message') }}
+                    </div>
+                    <button type="button" onclick="document.getElementById('alert').style.display = 'none';"
+                        class="text-red-50 hover:text-emerald-100 transition-all ease-in-out ms-auto">
+                        <i class="fa-solid fa-circle-xmark"></i>
+                    </button>
+                </div>
+            @endif
+
+            <div class="bg-white overflow-hidden border border-gray-200 md:rounded-2xl">
+                <div class="p-6 text-gray-900 space-y-5">
+                    <form method="POST" action="{{ route('programs.update', $program->id) }}" enctype="multipart/form-data">
+                        @csrf
+                        @method('PATCH')
+                        <div class="grid gap-5 mb-4 md:grid-cols-3">
+                            <div>
+                                <label for="code" class="block mb-2 text-sm font-medium text-gray-900">Kode
+                                    Jurusan</label>
+                                <input type="text" name="code" id="code" value="{{ $program->code }}"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                    placeholder="Kode Jurusan" required />
+                                <p class="mt-1 text-xs text-gray-500">
+                                    <span class="text-red-500 text-xs">{{ $errors->first('code') }}</span>
+                                </p>
+                            </div>
+                            <div>
+                                <label for="title" class="block mb-2 text-sm font-medium text-gray-900">Nama
+                                    Jurusan</label>
+                                <input type="text" name="title" id="title" value="{{ $program->title }}"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                    placeholder="Nama Jurusan" required />
+                                <p class="mt-1 text-xs text-gray-500">
+                                    <span class="text-red-500 text-xs">{{ $errors->first('title') }}</span>
+                                </p>
+                            </div>
+                            <div>
+                                <label for="level"
+                                    class="block mb-2 text-sm font-medium text-gray-900">Jenjang</label>
+                                <select id="level" name="level"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                                    <option value="{{ $program->level }}">{{ $program->level }}</option>
+                                    <option value="S1">S1</option>
+                                    <option value="D4">D4</option>
+                                    <option value="D3">D3</option>
+                                    <option value="Vokasi 2 Tahun">Vokasi 2 Tahun</option>
+                                </select>
+                                <p class="mt-1 text-xs text-gray-500">
+                                    <span class="text-red-500 text-xs">{{ $errors->first('level') }}</span>
+                                </p>
+                            </div>
+                            <div>
+                                <label for="campus"
+                                    class="block mb-2 text-sm font-medium text-gray-900">Kampus</label>
+                                <select id="campus" name="campus"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                                    <option value="{{ $program->campus }}">{{ $program->campus }}</option>
+                                    <option value="Kampus Tasikmalaya">Kampus Tasikmalaya</option>
+                                    <option value="Kampus Utama">Kampus Utama</option>
+                                    <option value="LP3I Tasikmalaya">LP3I Tasikmalaya</option>
+                                </select>
+                                <p class="mt-1 text-xs text-gray-500">
+                                    <span class="text-red-500 text-xs">{{ $errors->first('campus') }}</span>
+                                </p>
+                            </div>
+                            <div>
+                                <label for="type" class="block mb-2 text-sm font-medium text-gray-900">Tipe</label>
+                                <select id="type" name="type"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                                    <option value="" disabled {{ empty($program->type) ? 'selected' : '' }}>Pilih
+                                        Tipe</option>
+                                    <option value="R" {{ $program->type === 'R' ? 'selected' : '' }}>Reguler
+                                    </option>
+                                    <option value="N" {{ $program->type === 'N' ? 'selected' : '' }}>Non-Reguler
+                                    </option>
+                                    <option value="RPL" {{ $program->type === 'RPL' ? 'selected' : '' }}>Rekognisi
+                                        Pembelajaran Lampau</option>
+                                </select>
+                                <p class="mt-1 text-xs text-gray-500">
+                                    <span class="text-red-500 text-xs">{{ $errors->first('tipe') }}</span>
+                                </p>
+                            </div>
+                            <div>
+                                <label for="image" class="block mb-2 text-sm font-medium text-gray-900">Cover</label>
+                                <input type="file" name="image" id="image"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                                <p class="mt-1 text-xs text-gray-500">
+                                    <span class="text-red-500 text-xs">{{ $errors->first('image') }}</span>
+                                </p>
+                            </div>
+                        </div>
+                        <button type="submit"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl text-sm w-full sm:w-auto px-5 py-2.5 text-center space-x-1">
+                            <i class="fa-solid fa-floppy-disk"></i>
+                            <span>Simpan</span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
