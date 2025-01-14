84b6b952ac423335f71995f360a832c5ee6c1011
Lerian Febriana <kanglerian@gmail.com>
2025-01-14 09:13:12 +0700

    docs

--- resources/views/documentations/index.blade.php
+++ resources/views/documentations/index.blade.php
@@ -74,7 +74,6 @@
                                             class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                             {{ $no + 1 }}
                                         </th>
-                                        {{ $documentation->image }}
                                         <td class="px-6 py-4">
                                             <img src="{{ asset($documentation->image) }}"
                                                 alt="Documentations" class="h-20 object-cover rounded-xl">
